a8fa51dba80b8cda3357e31483ce305e9d9ad1ea
djc2105 <DCarlson19@shschools.org>
2025-04-28 19:49:05 -0400

    feat: add AWS Amplify authentication and data resources

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 
+Amplify.configure(outputs);
+
 export default function App() {
   return (
     <Authenticator>
